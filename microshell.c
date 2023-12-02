528fac36b1da4479d07f2230360365340d8367f7
Maksim Kramer <mkramer@c2r4p4.42quebec.com>
2023-12-02 17:54:45 -0500

    add solution from pascal

--- microshell.c
+++ microshell.c
@@ -4,42 +4,42 @@
 
 int tmp;
 
-int p(char *s)
+int err(char *s)
 {
 	while (*s)
 		write(2, s++, 1);
 	return 1;
 }
 
-int c(char **av, int i)
+int cd(char **av, int i)
 {
 	if (i != 2)
-		return p("error: cd: bad arguments\n");
+		return err("error: cd: bad arguments\n");
 	if (chdir(av[1]))
-		return p("error: cd: cannot change directory to ") & p(av[1]) & p("\n");
+		return err("error: cd: cannot change directory to ") & err(av[1]) & err("\n");
 	return 0;
 }
 
-int x(char **av, char **ep, int i)
+int exe(char **av, char **ep, int i)
 {
 	int tm2[2], r;
 	int pip = (av[i] && !strcmp(av[i], "|"));
 	if (pip && (pipe(tm2)))
-		return p("error: fatal\n");
+		return err("error: fatal\n");
 	int pid = fork();
 	if (!pid)
 	{
 		av[i] = 0;
 		if (dup2(tmp, 0) == -1 | close(tmp) == -1 | (pip && (dup2(tm2[1], 1)
 			== -1 | close(tm2[0]) == -1 | close(tm2[1]) == -1)))
-			return p("error: fatal\n");
+			return err("error: fatal\n");
 		execve(*av, av, ep);
-		return p("error: cannot execute ") & p(*av) & p("\n");
+		return err("error: cannot execute ") & err(*av) & err("\n");
 	}
 	if ((pip && (dup2(tm2[0], tmp) == -1 | close(tm2[0]) == -1
 		| close(tm2[1]) == -1)) | (!pip && dup2(0, tmp) == -1) |
 		waitpid(pid, &r, 0) == -1)
-		return p("error: fatal\n");
+		return err("error: fatal\n");
 	return WIFEXITED(r) && WEXITSTATUS(r);
 }
 
@@ -55,9 +55,11 @@ int main(int ac, char **av, char **ep)
 		while (av[i] && strcmp(av[i], "|") && strcmp(av[i], ";"))
 			i++;
 		if (!strcmp(*av, "cd"))
-			r = c(av, i);
+			r = cd(av, i);
 		else if (i)
-			r = x(av, ep, i);
+			r = exe(av, ep, i);
 	}
-	return ((dup2(0, tmp) == -1) && p("error: fatal\n")) | r;
+	return ((dup2(0, tmp) == -1) && err("error: fatal\n")) | r;
 }
+
+// leaks -atExit -- ./microshell /bin/ls "|" /usr/bin/grep microshell ";" /bin/echo i love my microshell 
